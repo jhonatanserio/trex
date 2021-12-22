5e949d87c8afcb5abbe99beb9cdd658d7d33e9b6
jhonatanserio <94911517+jhonatanserio@users.noreply.github.com>
2021-12-22 10:36:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
- var trex, trex_running, edges; 
+ var trex, trex_running,trexmimir, edges; 
 
  var groundImage;
 
@@ -7,6 +7,16 @@
  var nuvem
 
  var nuvemjpg
+
+ var fimdejogo=0
+
+ var jojo=1
+
+ var estadodojogo=jojo
+ 
+ var grupoverde
+
+ var grupobranco
 
  var chaojpg
 
@@ -23,6 +33,7 @@
   //oque carrega todas as imagems no jogo
 function preload(){
   trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+  trexmimir=loadAnimation("trex_collided.png")
   chaojpg=loadImage("ground2.png");
   nuvemjpg=loadImage("uvem.png");
   catus1=loadImage("obstacle1.png");
@@ -31,14 +42,17 @@ function preload(){
   catus4=loadImage("obstacle4.png");
   catus5=loadImage("obstacle5.png");
   catus6=loadImage("obstacle6.png");
+
 }
   //a propoção do cenario(n aumenta muito pq ai n carrega)
 function setup(){
   createCanvas(600,200);
-
+  grupoverde = new Group();
+  grupobranco = new Group();
   //criando o trex
  trex = createSprite(50,175,25,25);
  trex.addAnimation("meteoro",trex_running);
+ trex.addAnimation("planta",trexmimir);
   //adicione dimensão e posição ao trex
  trex.scale=0.5
   //o q defini onde chao fica (melhor nome ever)
@@ -53,25 +67,40 @@ function draw(){
   //definir a cor do plano de fundo 
   background ("white");
   
-  //pular quando tecla de espaço for pressionada(n da pra spamar)
-  if(keyDown("space")&&trex.y>160.5){
-  trex.velocityY=-10;
-  //mostra a altura do trex
-  console.log(trex.y);
-  //oque faz o trex n sai da tela caindo no void infinito
-   }
-  trex.velocityY=trex.velocityY +0.8;
-  trex.collide(chaopou2);
-  chao.velocityX=-10
-  //parte 2 do faz nuvem(to chamando ela)
-  uvem()
-  //oque faz o chão se infinito
-  drawSprites();
-  if(chao.x<0)
-  {
-    chao.x=chao.width/2
+  if(estadodojogo == jojo){
+          //pular quando tecla de espaço for pressionada(n da pra spamar)
+          if(keyDown("space")&&trex.y>160.5){
+          trex.velocityY=-10;
+          //mostra a altura do trex
+          console.log(trex.y);
+          //oque faz o trex n sai da tela caindo no void infinito
+          }
+          trex.velocityY=trex.velocityY +0.8;
+      
+          chao.velocityX=-10
+          //parte 2 do faz nuvem(to chamando ela)
+          uvem()
+          //oque faz o chão se infinito
+          
+          if(chao.x<0)
+          {
+            chao.x=chao.width/2
+          }
+          if(grupoverde.isTouching(trex)){
+            estadodojogo = fimdejogo
+          }
+          fazCactus();
+         
+}
+
+  else if(estadodojogo == fimdejogo){
+    chao.velocityX=0;
+    trex.velocityY=0;
+    trex.chargeAnimation("planta",trexmimir)
   }
- fazCactus();
+  trex.collide(chaopou2);
+  drawSprites();
+ 
 }
   //isso faz nuvem ou peido depende:D
 function uvem(){
@@ -84,6 +113,7 @@ function uvem(){
   nuvem.depth=trex.depth
   trex.depth=trex.depth
   nuvem.lifetime=360;
+  grupobranco.add(nuvem);
  }
 }
 function fazCactus(){
@@ -115,6 +145,9 @@ function fazCactus(){
     default:
       break;
   }
+  grupoverde.add(cactus);
 }
+
 }
+
 //bo morre nuvem
